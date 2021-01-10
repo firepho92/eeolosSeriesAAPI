617b553795c6b37feeb6225f3c2ac559d2271354
Alex Aguilar <alex@MacBook-Pro-de-Alex.local>
2021-01-10 12:28:23 -0600

    first commit

--- index.js
+++ index.js
@@ -5,7 +5,13 @@ const routes = require('./routes')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.use(cors())
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', req.headers.origin)
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+    res.header('Access-Control-Allow-Credentials', true)
+    next()
+})
 
 app.use('/', routes)
 
