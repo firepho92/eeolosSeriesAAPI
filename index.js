518a8defea20cac24becea8953b0d7aaad6ee3f0
Alex Aguilar <alex@MacBook-Pro-de-Alex.local>
2021-01-10 02:16:08 -0600

    first commit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const routes = require('./routes')
 const app = express()
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 3000
 
 app.use(cors())
 app.use(morgan('combined'))
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,9 @@
 const { Router } = require('express')
 const { getDataQuery } = require('../controllers/DataController')
-const axios = require('axios')
 
 const router = Router()
 
 router.get('/', async (req, res) => {
-  const response = await axios.get('http://localhost:3000/get')
-  console.log(response)
   res.status(200).send('ok')
 })
 
